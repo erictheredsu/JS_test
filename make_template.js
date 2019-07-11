ca0d52457fd36199a7f3b0874191b4510ad405fc
i070637 <eric.su@sap.com>
2019-07-11 14:34:37 +0800

    make csv template

--- make_template.js
+++ make_template.js
@@ -1,8 +1,23 @@
 const fs = require('fs');
 
+function printSchemaFile(table ,tableName){
+    let outline = '';
+     Object.keys(table).forEach(e => {
+        outline += e;
+        outline += ',';
+    });
+    outline = outline.substring(0,outline.lastIndexOf(','));
+    outline += "\r\n";
+
+    let output = fs.writeFileSync('./target/' + tableName + '.csv',outline );
+}
+
 let input = fs.readFileSync('./schema/items.json', 'utf8');
 let items = JSON.parse(input);
 let schemaArray = {};
+
+
+
 
 let props = Object.keys(items);
 for(let i=0;i< props.length;i++){
@@ -12,6 +27,20 @@ for(let i=0;i< props.length;i++){
     }
 }
 
-schemaArray.items = items;
+
+printSchemaFile(items, 'item');
+
+props = Object.keys(schemaArray)
+for(let i=0;i<props.length;i++){
+    if(schemaArray[props[i]] instanceof Array && schemaArray[props[i]].length >0 )
+    {
+        let row = schemaArray[props[i]][0];
+        printSchemaFile(row, props[i]);
+    }
+}
+
+
+
+
 
 console.log("pause");
